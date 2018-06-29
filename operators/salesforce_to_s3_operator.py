84bfb427db0e735ff17b275ba81deb30e7103860
daniel cohen <daniel@codeinvain.com>
2018-06-29 11:45:31 +0300

    removed comment

--- operators/salesforce_to_s3_operator.py
+++ operators/salesforce_to_s3_operator.py
@@ -6,7 +6,6 @@ from airflow.utils.decorators import apply_defaults
 from airflow.models import BaseOperator
 from airflow.hooks.S3_hook import S3Hook
 
-# from salesforce_plugin.hooks.salesforce_hook import SalesforceHook
 from airflow.contrib.hooks.salesforce_hook import SalesforceHook
 
 
